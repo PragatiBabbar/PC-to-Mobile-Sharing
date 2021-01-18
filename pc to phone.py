1f5e61339c094a3adf483d8b73cf6adadfa6f8fc
Pragati <68048816+PragatiBabbar@users.noreply.github.com>
2021-01-18 13:56:14 +0530

    Add files via upload

---
+++ pc to phone.py
@@ -0,0 +1,11 @@
+"""  Code by- @PragatiBabbar """
+
+
+# Execute this in your terminal ->  python -m http.server 8000
+
+# Copy your IP address.
+# (follow this steps: Go to System Preferences -> Network-> You will see your IP address there)
+
+# Type <IP_Address>:8000 in your mobile browser to access the files in the directory where you ran the above command.
+
+# Note:  Please check the attached screenshot.
